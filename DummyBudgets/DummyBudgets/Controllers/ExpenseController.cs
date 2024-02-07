dc28989969acc5c076b3879f97c7547b80547793
Steven Jagurić <steven.jaguric@gmail.com>
2024-02-07 19:50:05 +0100

    Create ExpenseController.cs

---
+++ DummyBudgets/DummyBudgets/Controllers/ExpenseController.cs
@@ -0,0 +1,45 @@
+﻿using DummyBudgets.Data;
+using DummyBudgets.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace DummyBudgets.Controllers
+{
+    public class ExpenseController : Controller
+    {
+        private readonly BudgetDbContext _dbContext;
+
+        public ExpenseController(BudgetDbContext dbContext)
+        {
+            this._dbContext = dbContext;
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            var expenses = _dbContext.Expenses.Include(e => e.Budget).ToList();
+            return View(expenses);
+        }
+
+        [HttpGet("create")]
+        public IActionResult Create()
+        {
+            ViewBag.Budgets = _dbContext.Budgets.ToList();
+            return View();
+        }
+
+        [HttpPost("create")]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Expense expense)
+        {
+            if (ModelState.IsValid)
+            {
+                _dbContext.Expenses.Add(expense);
+                _dbContext.SaveChanges();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewBag.Budgets = _dbContext.Budgets.ToList();
+            return View(expense);
+        }
+    }
+}
