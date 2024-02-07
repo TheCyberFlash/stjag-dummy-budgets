117bf08e1a0b00bb40508657a2f66c4b7bdd1255
Steven Jagurić <steven.jaguric@gmail.com>
2024-02-07 19:59:16 +0100

    Expense Index View Basic

---
+++ DummyBudgets/DummyBudgets/Views/Expense/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace DummyBudgets.Views.Expense
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
