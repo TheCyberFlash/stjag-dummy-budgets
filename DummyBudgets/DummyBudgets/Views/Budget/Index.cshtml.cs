7f434ab2693a4a91d9eb136c069f18e733378df0
Steven Jagurić <steven.jaguric@gmail.com>
2024-02-07 19:54:29 +0100

    Budget Index View Basic

---
+++ DummyBudgets/DummyBudgets/Views/Budget/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace DummyBudgets.Views.Budget
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
