7d123f664a43d0cd82e54f08e2bca1c06a43cb21
Steven Jagurić <steven.jaguric@gmail.com>
2024-02-07 19:48:06 +0100

    Create BudgetController.cs

---
+++ DummyBudgets/DummyBudgets/Controllers/BudgetController.cs
@@ -0,0 +1,43 @@
+﻿using DummyBudgets.Data;
+using DummyBudgets.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace DummyBudgets.Controllers
+{
+    public class BudgetController : Controller
+    {
+        private readonly BudgetDbContext _dbContext;
+
+        public BudgetController(BudgetDbContext dbContext)
+        {
+            this._dbContext = dbContext;
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            var budgets = _dbContext.Budgets.Include(b => b.Expenses).ToList();
+            return View(budgets);
+        }
+
+        [HttpGet("create")]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost("create")]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Budget budget)
+        {
+            if (ModelState.IsValid)
+            {
+                _dbContext.Budgets.Add(budget);
+                _dbContext.SaveChanges();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(budget);
+        }
+    }
+}
