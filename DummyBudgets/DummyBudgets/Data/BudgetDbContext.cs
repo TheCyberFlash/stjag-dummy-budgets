7015fc5a33031b2e815bdc02e71c18a3d96fce46
Steven Jagurić <steven.jaguric@gmail.com>
2024-02-07 19:43:33 +0100

    Create BudgetDbContext.cs

---
+++ DummyBudgets/DummyBudgets/Data/BudgetDbContext.cs
@@ -0,0 +1,13 @@
+﻿using DummyBudgets.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DummyBudgets.Data
+{
+    public class BudgetDbContext : DbContext
+    {
+        public DbSet<Budget> Budgets { get; set; }
+        public DbSet<Expense> Expenses { get; set; }
+
+        public BudgetDbContext(DbContextOptions<BudgetDbContext> options) : base(options) { }
+    }
+}
