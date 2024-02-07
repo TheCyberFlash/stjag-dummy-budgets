7035a011ec2d007b9490ff416687aa1ff7f9a6dd
Steven Jagurić <steven.jaguric@gmail.com>
2024-02-07 19:41:57 +0100

    Create Expense.cs

---
+++ DummyBudgets/DummyBudgets/Models/Expense.cs
@@ -0,0 +1,12 @@
+﻿namespace DummyBudgets.Models
+{
+    public class Expense
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public decimal Amount { get; set; }
+        public DateTime Date { get; set; }
+        public int BudgetId { get; set; }
+        public Budget Budget { get; set; }
+    }
+}
